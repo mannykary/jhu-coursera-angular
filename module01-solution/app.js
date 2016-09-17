66f0026afd4fde85a65ea1ac6b13a4fa5f21affe
Manny Karyampudi <mrkaryam@gmail.com>
2016-09-16 20:07:46 -0700

    More advanced stuff

--- module01-solution/app.js
+++ module01-solution/app.js
@@ -6,8 +6,9 @@
 
   LunchCheckController.$inject = ['$scope'];
   function LunchCheckController($scope) {
-    $scope.dishes = [];
+    $scope.dishes = '';
     $scope.message = '';
+    $scope.style = {};
 
     $scope.checkIfTooMuch = function () {
       if ($scope.dishes) {
@@ -18,15 +19,33 @@
 
         if (numDishes > 3) {
           $scope.message = 'Too much!';
+          $scope.setColor('green');
         } else if (numDishes > 0 && numDishes <= 3) {
           $scope.message = 'Enjoy!';
+          $scope.setColor('green');
         } else {
           // This should never happen, but throw error if it does.
           throw 'numDishes is invalid';
         }
       } else {
         $scope.message = 'Please enter data first';
+        $scope.setColor('red');
       }
     };
+
+    $scope.setColor = function(color) {
+      $scope.style.textColor = color;
+
+      switch(color) {
+        case 'red':
+          $scope.style.validation = 'error';
+          break;
+        case 'green':
+          $scope.style.validation = 'valid';
+          break;
+        default:
+          $scope.style.validation = '';
+      }
+    }
   }
 })();
